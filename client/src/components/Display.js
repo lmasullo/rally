27b0923e3b2bcd1acfbe9e9ad963ff9507121921
Thai Phan <thaiphan90@yahoo.com>
2019-10-21 17:17:38 -0500

    added display and home page

---
+++ client/src/components/Display.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+
+class Display extends Component {
+
+    render() {
+        return (
+            <div>
+                <div className="container-fluid">
+                    <div className="row">
+                        <nav>
+                            <div className="col-2 title">
+                                <h4>Racquet Ralley</h4>
+                            </div>
+                            <div className="col-10 right">
+                                <button className="btn">Login</button>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 card" id="banner">
+                            
+                        </div>
+                        <div className="col-12 card" id="instructions">
+                            
+                        </div>
+                        <div className="col-12 card" id="about">
+                            
+                        </div>
+                    </div>
+                </div>
+
+                <div className="container-fluid">
+                    <div className="row">
+                        <footer className="col-12">
+                            
+                        </footer>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Display;
---
+++ client/src/components/Home.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class Home extends Component {
+    
+}
+
+export default Home;
