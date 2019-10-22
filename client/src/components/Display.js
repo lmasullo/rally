3463281e7f38a2217a752b523c4bd6cdb26965b8
Thai Phan <thaiphan90@yahoo.com>
2019-10-22 18:25:45 -0500

    changed to functional component

--- client/src/components/Display.js
+++ client/src/components/Display.js
@@ -1,58 +1,58 @@
-import React, { Component } from "react";
+import React from "react";
 import axios from "axios";
 import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 
-class Display extends Component {
+const Display = () => {
 
     handleLogin()
 
-    render() {
+    return (
+        <div>
 
-        return (
-            <div>
-
-                <div className="container-fluid">
-                    <div className="row">
-                        <nav>
-                            <div className="col-2 float-left">
-                                <h2>Racquet Ralley</h2>
-                            </div>
-                            <div className="col-10 float-right">
-                                <button href="#" className="btn btn-primary" onclick={handleLogin}>Login</button>
-                            </div>
-                        </nav>
-                    </div>
+            <div className="container-fluid">
+                <div className="row">
+                    <nav>
+                        <div className="col-2 float-left">
+                            <h2>Racquet Ralley</h2>
+                        </div>
+                        <div className="col-10 float-right">
+                            <Link to='/home'>
+                            <button href="#" className="btn btn-primary">Login</button>
+                            </Link>
+                        </div>
+                    </nav>
                 </div>
+            </div>
 
-                <div className="container">
-                    <div className="row">
+            <div className="container">
+                <div className="row">
 
-                        <div className="col-12 card" id="banner">
-                            
-                        </div>
-
-                        <div className="col-12 card" id="instructions">
-                            
-                        </div>
-
-                        <div className="col-12 card" id="about">
-                            
-                        </div>
+                    <div className="col-12 card" id="banner">
                         
                     </div>
-                </div>
 
-                <div className="container-fluid">
-                    <div className="row">
-                        <footer className="col-12">
-                        <p>Copyright &copy; 2019 Team Racquet Rally.</p>
-                        </footer>
+                    <div className="col-12 card" id="instructions">
+                        
                     </div>
-                </div>
 
+                    <div className="col-12 card" id="about">
+                        
+                    </div>
+
+                </div>
             </div>
-        );
-    };
+
+            <div className="container-fluid">
+                <div className="row">
+                    <footer className="col-12">
+                    <p>Copyright &copy; 2019 Team Racquet Rally.</p>
+                    </footer>
+                </div>
+            </div>
+
+        </div>
+    );
 };
+
 
 export default Display;
