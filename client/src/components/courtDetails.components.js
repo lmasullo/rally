82cc55048d03275d5ff377cb23d3ca72fa58abf9
Thai Phan <thaiphan90@yahoo.com>
2019-10-26 13:19:58 -0500

    added courtDetails.component

---
+++ client/src/components/courtDetails.components.js
@@ -0,0 +1,108 @@
+//Dependencies
+import React , { Component } from "react";
+import axios from "axios";
+import {Redirect} from 'react-router-dom';
+
+//! Is this the correct way????????
+//Check if production or local
+let API_URL = '';
+if (process.env.NODE_ENV === 'production') {
+    API_URL = 'https://racquet-rally.herokuapp.com/home';
+}else{
+    API_URL = 'http://localhost:4000/home';
+}
+
+
+//Class Component
+export default class Details extends Component {   
+
+    //Set state and bindings
+    constructor(props){
+        super(props);
+        this.state = {
+            userName: '',
+            redirect: '',
+        };
+    }
+
+    componentDidMount(){        
+        axios.get(API_URL, { withCredentials: true })
+        .then(response => {
+            console.log(response);
+            //User Not logged in, so redirect to login, by setting redirect to true, it triggers in render
+            if(response.data === 'Not Logged In!'){
+                console.log('no data');
+                this.setState({
+                redirect: true
+                })
+            }else{
+                //User Logged in
+                this.setState({
+                users: response.data
+                })
+            }    
+        })
+        .catch(err =>{
+            console.log(err);        
+        })
+    }
+
+        render(){
+            //Check if redirect state is true
+            if (this.state.redirect){
+                return <Redirect to="/" />;
+            }
+            return (
+                <div>
+                   
+                    <div class="jumbotron jumbotron-fluid">
+                        <div class="container">
+                            <h1 class="display-4">{this.props.centers.centerName}</h1>
+                            <p class="lead">Welcome!</p>
+                        </div>
+                    </div>
+
+                    <div className='container'>
+                        <div className='row'>
+                            <table class="col-12 table">
+                                <tbody>
+                                    
+                                    <tr>
+                                    <th scope="row">Address</th>
+                                    <td>{this.props.centers.addressLink}</td>
+                                    </tr>
+
+                                    <tr>
+                                    <th scope="row">Hours</th>
+                                    <td>{this.props.centers.hours}</td>
+                                    </tr>
+
+                                    <tr>
+                                    <th scope="row">Number of Courts</th>
+                                    <td>{this.props.centers.numCourts}</td>
+                                    </tr>
+
+                                    <tr>
+                                    <th scope="row">Cost (Free?)</th>
+                                    <td>{this.props.centers.cost}</td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                    <div className="container-fluid">
+                        <div className="row">
+                            <footer className="col-12">
+                            <p>Copyright &copy; 2019 Team Racquet Rally.</p>
+                            </footer>
+                        </div>
+                    </div>
+
+                </div>
+            );
+        };
+
+    };
+
