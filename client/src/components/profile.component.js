5ae6a44cdb03b93ba03aeadb67e76306c251cd2b
Pheorix <jrix21@gmail.com>
2019-11-05 18:31:57 -0600

    Update profile.component.js

--- client/src/components/profile.component.js
+++ client/src/components/profile.component.js
@@ -1,134 +1,180 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Redirect } from 'react-router-dom';
+// Dependencies
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
+// Import the centerCard component
+import Center from "./centerCard.component";
 
 // Check if production or local
-let API_URL = '';
-if (process.env.NODE_ENV === 'production') {
-  API_URL = 'https://racquet-rally.herokuapp.com/user/';
+let API_URL = "";
+if (process.env.NODE_ENV === "production") {
+  API_URL = "https://racquet-rally.herokuapp.com/";
 } else {
-  API_URL = 'http://localhost:4000/user/';
+  API_URL = "http://localhost:4000/";
 }
 
-// CSS Styles
-const styleHTML = {
-  height: '100%',
-};
-
 const styleBody = {
-  height: '100%',
-  display: 'flex',
-  msFlexAlign: 'center',
-  alignItems: 'center',
-  paddingTop: '40px',
-  paddingBottom: '40px',
-  backgroundColor: '#f5f5f5',
+  height: "100%",
+  display: "flex",
+  msFlexAlign: "center",
+  alignItems: "center",
+  paddingTop: "40px",
+  paddingBottom: "40px",
+  backgroundColor: "#f5f5f5"
 };
 
 const styleForm = {
-  width: '100%',
-  maxWidth: '330px',
-  margin: 'auto',
-  position: 'relative',
-  boxSizing: 'border-box',
-  height: 'auto',
-  padding: '10px',
-  fontSize: '16px',
-  zIndex: '2',
+  width: "100%",
+  maxWidth: "600px",
+  margin: "auto",
+  position: "relative",
+  boxSizing: "border-box",
+  height: "auto",
+  padding: "10px",
+  fontSize: "16px",
+  zIndex: "2",
+  textAlign: "center"
 };
 
-// Functional component
-const Center = props => <span>{props.centers}</span>;
+// Method to display each Center's Card on the page
+// This is called in the return statement at the bottom of the page
+function centerList(centers, onChangeSaveCenter) {
+  // Loop over the centers array
+  return centers.map(currentCenter => (
+    // Send props to the centerCard component, imported from components
+    <Center
+      key={currentCenter._id}
+      centers={currentCenter}
+      centerId={currentCenter._id}
+      isFavorite={currentCenter.isFavorite}
+      onChangeSaveCenter={onChangeSaveCenter}
+    />
+  ));
+}
 
-class Login extends Component {
-  // Set state and bindings
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: [],
-      redirect: '',
-    };
-  }
+// Functional Component with Hooks
+function Profile() {
+  // Set initial State with Hooks
+  const [centers, setCenters] = useState([]);
+  const [redirect, setRedirect] = useState("");
+  const [user, setUser] = useState([]);
+  const [arrayLength, setArrayLength] = useState(0);
 
-  // Get user info from authentication
-  componentDidMount() {
-    axios.get(`${API_URL}auth`, { withCredentials: true }).then(response => {
-      // console.log(response.data);
-      // User Not logged in, so redirect to login, by setting redirect to true, it triggers in render
-      if (response.data === 'Not Logged In!') {
-        console.log('no data');
-        this.setState({
-          redirect: true,
+  // Get all the centers when the component mounts and put in centers
+  // Use useEffect instead of ComponentDidMount
+  useEffect(() => {
+    // Use async await to fetch centers and users
+    async function go() {
+      try {
+        // Axios calls for centers and current user
+        const centerPromise = axios(`${API_URL}center`, {
+          withCredentials: true
         });
-      } else {
-        // User Logged in
-        // console.log('Response', response.data);
-        this.setState({
-          user: response.data,
+        const userPromise = axios(`${API_URL}user/auth`, {
+          withCredentials: true
         });
+
+        // await both promises to come back and destructure the result into their own variables
+        const [centersData, userData] = await Promise.all([
+          centerPromise,
+          userPromise
+        ]);
+
+        // Check if user logged in
+        if (centersData.data === "Not Logged In!") {
+          console.log("No Data");
+          setRedirect(true);
+        } else {
+          // User Logged in
+          // Maps over the centers array and checks if it is in the user's centers array (using id) and then adds isFavorite true/false
+          centersData.data = centersData.data.map(c => {
+            const isFavorite = userData.data.centers.some(uc => uc === c._id);
+            return { isFavorite, ...c };
+          });
+          // Set centers state
+          setCenters(centersData.data);
+
+          // Set the user object in state
+          setUser(userData.data);
+
+          // Check if the user has a skill level entered, if not show toast
+        }
+      } catch (e) {
+        console.error(e); // 💩
       }
-    });
+    }
+    // Call the async/await function
+    go();
+    // Use arrayLength state to tell react to re-run useEffect after each change of the user's array
+  }, [arrayLength]); // End Use Effect
+
+  // Function when user clicks Save Me checkbox on center card
+  function onChangeSaveCenter(e, centerId) {
+    // See if the clicked center is already a favorite of the user
+    const checkCenter = e.target.checked;
+
+    // Create object of the center's id to send to backend
+    const newCenter = {
+      id: centerId
+    };
+
+    // If not already a favorite, push to an array and then send to the home route to save
+    if (checkCenter === true) {
+      // Send to back-end to Update user's centers
+      axios
+        .post(`${API_URL}user/update/center/${user._id}`, newCenter)
+        .then(res => {
+          // Set the setArrayLength state to re-trigger useEffect and re-render cards
+          setArrayLength(res.data.centers.length);
+        })
+        .catch(err => console.log(err));
+    } else {
+      // Delete center
+      // Send to back-end to Update user's centers
+      axios
+        .post(`${API_URL}user/delete/center/${user._id}`, newCenter)
+        .then(res => {
+          // Set the setArrayLength state to re-trigger useEffect and re-render cards
+          setArrayLength(res.data.centers.length);
+        })
+        .catch(err => console.log(err));
+    }
   }
 
-  // Method to display each element in the table
-  centerList() {
-    console.log(this.state);
-
-    if (this.state.user.centers) {
-      // Loop over the centers array
-      return this.state.user.centers.map(currentCenter => {
-        console.log(currentCenter);
-
-        // Return the User component, pass some props to the User Component
-        // The User component is above in this file as a functional component
-        return <Center centers={currentCenter} key={currentCenter} />;
-      });
-    }
-    return 'Loading...';
+  // Check if redirect state is true
+  if (redirect) {
+    return <Redirect to="/" />;
   }
 
-  render() {
-    // Check if redirect state is true
-    if (this.state.redirect) {
-      return <Redirect to="/" />;
-    }
-
-    return (
-      <div style={styleHTML}>
-        <div style={styleBody} className="text-center">
+  return (
+    <div style={styleBody}>
+      <div className="container">
+        <div className="row">
           <form className="form-signin" style={styleForm}>
-            <h1 className="h3 mb-3 font-weight-normal">Signed In User:</h1>
-            <h2>{this.state.user.name}</h2>
-            <h3>{this.state.user.email}</h3>
-            <img
-              src={this.state.user.image}
-              alt="User"
-              height="100"
-              width="100"
-            />
+            <h1 className="h3 mb-3 font-weight-normal">View your Profile!</h1>
+            <h2>{user.name}</h2>
+            <h3>{user.email}</h3>
+            <img src={user.image} alt="User" height="100" width="100" />
 
             <h3>
               <label>Skill Level: </label>
-              {this.state.user.skillLevel}
+              {user.skillLevel}
             </h3>
-            {this.centerList()}
-            {/* <button href={`/edit/${this.state.user._id}`} className="btn btn-primary">Edit Info</button> */}
-            {/* <Link to={`/edit/${this.state.user._id}`}>Edit</Link> */}
-            <a
-              className="btn btn-primary"
-              href={`/edit/${this.state.user._id}`}
-              role="button"
-            >
-              Edit Profile
-            </a>
-            <p className="mt-5 mb-3">
-              Copyright &copy; 2019 Team Racquet Rally.
-            </p>
           </form>
+          <div className="col-12">
+            {/* Toast, send show and message prop */}
+
+            <h3 className="text-center mb-4">
+              {user.name}'s' Favorite Courts!
+            </h3>
+          </div>
         </div>
+        {/* Call centerList function to map over the centers and render the cards */}
+        {/* Pass in the centers, onChangeSaveCenter Function */}
+        <div className="row">{centerList(centers, onChangeSaveCenter)}</div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default Login;
+export default Profile;
